b368b26bf43233b311ed2418b8f79198f3b3166a
notapresent <notapresent@gmail.com>
2017-10-23 09:07:02 +0300

    Implement redirect

--- src/main/java/io/github/notapresent/URLFetchSession.java
+++ src/main/java/io/github/notapresent/URLFetchSession.java
@@ -1,27 +1,47 @@
 package io.github.notapresent;
 
-import com.google.appengine.api.urlfetch.HTTPRequest;
-import com.google.appengine.api.urlfetch.HTTPResponse;
-import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.*;
+import com.sun.istack.internal.NotNull;
+import com.sun.javaws.exceptions.InvalidArgumentException;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.Future;
 
 
 public class URLFetchSession implements URLFetchService {
+    public static final int MAX_REDIRECTS = 5;
     private URLFetchService service;
+
 
     public URLFetchSession(URLFetchService service) {
         this.service = service;
     }
 
     public HTTPResponse fetch(URL url) throws IOException {
-        return service.fetch(url);
+        HTTPRequest req = new HTTPRequest(url, HTTPMethod.GET,
+                FetchOptions.Builder.doNotFollowRedirects());
+        return doRequest(req, true);
     }
 
     public HTTPResponse fetch(HTTPRequest req) throws IOException {
-        return service.fetch(req);
+        boolean followRedirects = req.getFetchOptions().getFollowRedirects();
+        return doRequest(prepRequest(req), followRedirects);
+    }
+
+    private HTTPResponse doRequest(HTTPRequest req, boolean followRedirects) throws IOException {
+        int numHops = 0;
+        @NotNull
+        HTTPResponse resp = null;
+        while(++numHops < MAX_REDIRECTS) {
+            resp = service.fetch(req);
+            if (!followRedirects || resp.getResponseCode() == 200) {
+                break;
+            }
+            req = makeRedirectRequest(req.getURL(), resp);
+        }
+        return resp;
     }
 
     public Future<HTTPResponse> fetchAsync(URL url) {
@@ -29,6 +49,68 @@ public class URLFetchSession implements URLFetchService {
     }
 
     public Future<HTTPResponse> fetchAsync(HTTPRequest req) {
+        FetchOptions origFetchOpts = req.getFetchOptions();
+        FetchOptions newFetchOpts = copyFetchOptions(origFetchOpts);
         return service.fetchAsync(req);
+    }
+
+    public static HTTPRequest makeRedirectRequest(URL originalUrl, HTTPResponse resp)
+            throws MalformedURLException {
+        String location = getHeader(resp, "location");
+
+        if(location == null) {
+            throw new IllegalArgumentException("Redirect response withuut location header");
+        }
+        return new HTTPRequest(new URL(originalUrl, location),
+                HTTPMethod.GET,
+                FetchOptions.Builder.doNotFollowRedirects());
+    }
+
+    public static boolean isRedirect(int code) {
+        return code >= 301 && code <= 303;
+    }
+
+    public static String getHeader(HTTPResponse resp, String  name) {
+        for(HTTPHeader hdr: resp.getHeaders()) {
+            if(hdr.getName().equalsIgnoreCase(name)) {
+                return hdr.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static FetchOptions copyFetchOptions(FetchOptions src) {
+        FetchOptions dest = FetchOptions.Builder.withDefaults();
+        if(src.getAllowTruncate()) {
+            dest.allowTruncate();
+        } else {
+            dest.disallowTruncate();
+        }
+        if(src.getFollowRedirects()) {
+            dest.followRedirects();
+        } else {
+            dest.doNotFollowRedirects();
+        }
+        dest.setDeadline(src.getDeadline());
+
+        if(src.getValidateCertificate()) {
+            dest.validateCertificate();
+        } else {
+            dest.doNotValidateCertificate();
+        }
+        return dest;
+    }
+
+    public static HTTPRequest copyWithFetchOptions(HTTPRequest original, FetchOptions opts) {
+        HTTPRequest rv = new HTTPRequest(original.getURL(), original.getMethod(), opts);
+        for(HTTPHeader hdr : original.getHeaders()) {
+            rv.setHeader(hdr);
+        }
+        return rv;
+    }
+
+    public static HTTPRequest prepRequest(HTTPRequest origReq) {
+        FetchOptions newFetchOpts = copyFetchOptions(origReq.getFetchOptions()).doNotFollowRedirects();
+        return copyWithFetchOptions(origReq, newFetchOpts);
     }
 }
--- src/test/java/io/github/notapresent/URLFetchSessionTest.java
+++ src/test/java/io/github/notapresent/URLFetchSessionTest.java
@@ -1,29 +1,32 @@
 package io.github.notapresent;
 
-import com.google.appengine.api.urlfetch.HTTPRequest;
-import com.google.appengine.api.urlfetch.HTTPResponse;
-import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.*;
+import com.google.appengine.repackaged.com.google.api.client.http.HttpResponse;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class URLFetchSessionTest {
     @Mock
     private URLFetchService mockService;
-    @Mock
-    private HTTPResponse mockResponse;
 
     private URLFetchSession session;
 
@@ -31,15 +34,55 @@ public class URLFetchSessionTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         session = new URLFetchSession(mockService);
-        when(mockService.fetch(any(URL.class))).thenReturn(mockResponse);
     }
 
     @Test
-    public void testFetchForwards() throws MalformedURLException, IOException {
+    public void testFetchForwards() throws IOException {
+        HTTPResponse mockResp = makeMockResponse();
+        when(mockService.fetch(any(HTTPRequest.class))).thenReturn(mockResp);
+
         HTTPResponse resp = session.fetch(new URL("http://fake.url"));
-        // assert service.fetch was called
-        verify(mockService, times(1)).fetch(any(URL.class));
-        // assert response returned
-        assertSame(resp, mockResponse);
+        verify(mockService, times(1)); // .fetch(any(URL.class));
+        assertSame(resp, mockResp);
+    }
+
+    @Test
+    public void testCopyFetchOptions(){
+        // all non-default options
+        FetchOptions src = FetchOptions.Builder.withDefaults()
+                .allowTruncate()
+                .doNotFollowRedirects()
+                .setDeadline(33.33)
+                .validateCertificate();
+        FetchOptions dest = URLFetchSession.copyFetchOptions(src);
+        assertEquals(src.getFollowRedirects(), dest.getFollowRedirects());
+        assertEquals(src.getAllowTruncate(), dest.getAllowTruncate());
+        assertEquals(src.getDeadline(), dest.getDeadline());
+        assertEquals(src.getValidateCertificate(), dest.getValidateCertificate());
+    }
+
+    @Test
+    public void testRedirectRedirects() throws IOException {
+        List<HTTPHeader> redirHdrs = Collections.singletonList(
+                new HTTPHeader("location", "http://redir.url"));
+        HTTPResponse redirResp = makeMockResponse("Redirecting", 302, redirHdrs);
+        HTTPResponse okResp = makeMockResponse();
+        when(mockService.fetch(any(HTTPRequest.class))).thenReturn(redirResp, okResp);
+
+        HTTPResponse resp = session.fetch(new URL("http://fake.url"));
+        assertSame(resp, okResp);
+        verify(mockService, times(2)).fetch(any(HTTPRequest.class));
+    }
+
+    private HTTPResponse makeMockResponse(String content, int statusCode, List<HTTPHeader> headers) {
+        HTTPResponse rv = mock(HTTPResponse.class);
+        when(rv.getResponseCode()).thenReturn(statusCode);
+        when(rv.getContent()).thenReturn(content.getBytes());
+        when(rv.getHeaders()).thenReturn(headers == null ? new ArrayList<>() : headers);
+        return rv;
+    }
+
+    private HTTPResponse makeMockResponse() {
+        return makeMockResponse("Dummy vontent", 200, null);
     }
 }
