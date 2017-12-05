ca4be845adde214c05fff779a45f730a5a97bc63
notapresent <notapresent@gmail.com>
2017-12-06 02:44:43 +0300

    Implement tests for Status

---
+++ common/src/test/java/io/github/notapresent/usersampler/common/StatusTest.java
@@ -0,0 +1,24 @@
+package io.github.notapresent.usersampler.common;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StatusTest {
+    enum CustomStatus implements Status {
+        TEST;
+    }
+
+    @Test
+    public void toIntShouldReturnGenericStatusValueUnchanged() {
+        GenericStatus gst = GenericStatus.PRIVATE;
+        assertEquals(gst.ordinal(), gst.toInt());
+    }
+
+    @Test
+    public void toIntShouldOffsetSiteStatusValueBySizeOfGenericStatus() {
+        CustomStatus customStatus = CustomStatus.TEST;
+        assertEquals(GenericStatus.values().length, customStatus.toInt());
+
+    }
+}
