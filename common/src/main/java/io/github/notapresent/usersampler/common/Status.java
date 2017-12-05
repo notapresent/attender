3994463780390a4a41449da65f85ca05dd70f5fa
notapresent <notapresent@gmail.com>
2017-12-06 02:12:24 +0300

    Better variable name

--- common/src/main/java/io/github/notapresent/usersampler/common/Status.java
+++ common/src/main/java/io/github/notapresent/usersampler/common/Status.java
@@ -4,15 +4,14 @@ package io.github.notapresent.usersampler.common;
 public interface Status {
     /* Mimic enum values */
     String name();
-
     int ordinal();
 
     default int toInt() {
-        int genericSize = GenericStatus.values().length;
+        int minCustomStatus = GenericStatus.values().length;
         if (this instanceof GenericStatus) {
             return ordinal();
         } else {
-            return genericSize + ordinal();
+            return minCustomStatus + ordinal();
         }
     }
 }
