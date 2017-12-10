61da88ce9877850b205fa3da87b6d28a033d638f
notapresent <notapresent@gmail.com>
2017-12-10 07:33:31 +0300

    Remove stray code

--- common/src/test/java/io/github/notapresent/usersampler/common/sampling/SinglePlexerTest.java
+++ common/src/test/java/io/github/notapresent/usersampler/common/sampling/SinglePlexerTest.java
@@ -1,24 +1,6 @@
 package io.github.notapresent.usersampler.common.sampling;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 public class SinglePlexerTest {
-    A a;
 
-    @Before
-    public void setUp() {
-        a = new A();
-    }
-
-    @Test
-    public void multiSend() {
-        assertEquals("Something", a.val);
-    }
 }
 
-class A {
-    public String val = "Something";
-}
