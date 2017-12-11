f2ca367d2488c0a4c89ad36ef9fa0c9e71d354f0
notapresent <notapresent@gmail.com>
2017-12-11 07:18:58 +0300

    Objectify setup

--- gaeapp/src/main/java/io/github/notapresent/usersampler/gaeapp/ServicesModule.java
+++ gaeapp/src/main/java/io/github/notapresent/usersampler/gaeapp/ServicesModule.java
@@ -4,6 +4,8 @@ import com.google.appengine.api.urlfetch.URLFetchService;
 import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.googlecode.objectify.ObjectifyFilter;
 import io.github.notapresent.usersampler.common.HTTP.RequestFactory;
 import io.github.notapresent.usersampler.common.HTTP.Session;
 import io.github.notapresent.usersampler.gaeapp.HTTP.URLFetchCookieManager;
@@ -15,6 +17,8 @@ public class ServicesModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(RequestFactory.class);
+        bind(ObjectifyFilter.class).in(Singleton.class);
+
         bind(Session.class).to(URLFetchSession.class);
         bind(CookieHandler.class).to(URLFetchCookieManager.class);
     }
--- gaeapp/src/main/java/io/github/notapresent/usersampler/gaeapp/ServletConfig.java
+++ gaeapp/src/main/java/io/github/notapresent/usersampler/gaeapp/ServletConfig.java
@@ -1,10 +1,12 @@
 package io.github.notapresent.usersampler.gaeapp;
 
 import com.google.inject.servlet.ServletModule;
+import com.googlecode.objectify.ObjectifyFilter;
 
 class ServletConfig extends ServletModule {
     @Override
     protected void configureServlets() {
+        filter("/*").through(ObjectifyFilter.class);
         serve("/sampler").with(SamplerServlet.class);
     }
 }
