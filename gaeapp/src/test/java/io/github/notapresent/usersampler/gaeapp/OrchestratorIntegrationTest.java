1398ad0f02838b548cd57f5499386ecb5107e58f
notapresent <notapresent@gmail.com>
2017-12-14 11:44:46 +0300

    Reenable integration test

--- gaeapp/src/test/java/io/github/notapresent/usersampler/gaeapp/OrchestratorIntegrationTest.java
+++ gaeapp/src/test/java/io/github/notapresent/usersampler/gaeapp/OrchestratorIntegrationTest.java
@@ -47,7 +47,7 @@ public class OrchestratorIntegrationTest {
         helper.tearDown();
     }
 
-    // @Test    // TODO uncomment when adapters are ready
+    @Test    // TODO uncomment when adapters are ready
     public void itShouldCreateOneSamplePerAdapter() {
         Provider<Objectify> ofyProvider = ObjectifyService::ofy;
         SampleStorage storage = new GAESampleStorage(
