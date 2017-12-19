df2b82bd28f9580ca97a30710037af8e64175de7
notapresent <notapresent@gmail.com>
2017-12-19 15:39:14 +0300

    Implement SamplePayloadCompactor

---
+++ gaeapp/src/main/java/io/github/notapresent/usersampler/gaeapp/storage/SamplePayloadCompactor.java
@@ -0,0 +1,36 @@
+package io.github.notapresent.usersampler.gaeapp.storage;
+
+import io.github.notapresent.usersampler.common.sampling.UserStatus;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class SamplePayloadCompactor {
+    public static Map<UserStatus, List<String>> deflate(Map<String, UserStatus> payload) {
+        return payload.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getValue,
+                        (e) -> new ArrayList<String>(Collections.singletonList(e.getKey())),
+                        (left, right) ->  {
+                            left.addAll(right);
+                            return left;
+                        }
+                        )
+                );
+    }
+
+    public static Map<String, UserStatus> inflate(Map<UserStatus, List<String>> deflated) {
+
+        return deflated.entrySet().stream()
+                .flatMap(
+                        (e) ->  e.getValue().stream()
+                                .map((s) -> new AbstractMap.SimpleEntry<>(e.getKey(), s))
+                )
+                .collect(Collectors.toMap(
+                        Map.Entry::getValue,
+                        Map.Entry::getKey
+                        )
+
+                );
+    }
+}
