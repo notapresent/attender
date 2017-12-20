4b507774d99942b8267b2620cd3c2a683b665628
notapresent <notapresent@gmail.com>
2017-12-20 19:11:00 +0300

    Add environment dump

--- gaeapp/src/main/java/io/github/notapresent/usersampler/gaeapp/SysinfoServlet.java
+++ gaeapp/src/main/java/io/github/notapresent/usersampler/gaeapp/SysinfoServlet.java
@@ -3,6 +3,8 @@ package io.github.notapresent.usersampler.gaeapp;
 import com.google.appengine.api.utils.SystemProperty;
 import com.google.inject.Singleton;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,15 +16,15 @@ public class SysinfoServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
-
+    String divider = String.join("", Collections.nCopies(80, "-")) + "\n";
     Properties properties = System.getProperties();
+    Map<String, String> env = System.getenv();
 
     response.setContentType("text/plain; charset=utf-8");
-    String message = "App Engine Standard using %s%n" +
-        "Java %s%n";
-
     StringBuilder sb = new StringBuilder();
-    sb.append("Java System properties\n-------------------------\n");
+
+    sb.append("Java System properties\n");
+    sb.append(divider);
     for (String key : properties.stringPropertyNames()) {
       sb.append(key)
           .append(": ")
@@ -30,14 +32,19 @@ public class SysinfoServlet extends HttpServlet {
           .append("\n");
     }
 
-    sb.append("\n\nAppengine SystemProperties\n---------------------------\n");
-    sb
-        .append("SystemProperty.Environment: ")
+    sb.append("\n\nAppengine SystemProperties\n");
+    sb.append(divider);
+    sb.append("SystemProperty.Environment: ")
         .append(SystemProperty.environment.value())
         .append("\napplicationId: ").append(SystemProperty.applicationId.get())
         .append("\napplicationVersion: ").append(SystemProperty.applicationVersion.get())
         .append("\nversion: ").append(SystemProperty.version.get());
 
-    response.getWriter().format(sb.toString());
+
+    sb.append("\n\nEnvironment variables\n");
+    sb.append(divider);
+    env.forEach((key, value) -> sb.append(String.format("%s: %s\n", key, value)));
+
+    response.getWriter().print(sb.toString());
   }
 }
